99ab63eee35e6d491179ec6c096e3e9677113efe
NvTai <nvtai.050198@gmail.com>
2021-10-23 14:00:00 +0700

    nodejs_soketIO_flutter

---
+++ nodejs_socketIO_flutter/flutter_socketio/socketio_fluter/android/app/src/main/kotlin/com/example/socketio_fluter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.socketio_fluter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- nodejs_socketIO_flutter/server/index.js
+++ nodejs_socketIO_flutter/server/index.js
@@ -1,3 +1,23 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const options = { /* ... */ };
+const io = require('socket.io')(server, options);
+const port = 3484;
+
+io.on('connection', client => {
+    console.log(`New client connected`); 
+    client.on('fromclient',data =>{ 
+        console.log(data);
+        client.emit('fromSever',`${Number(data)+1}`)
+    })
+    client.on('disconnect',() =>console.log(`Client disconnected`))
+});
+
+server.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+/*
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,5 +29,5 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
+*/
 
